5165469f94800d4ed0be753ca113e26290736684
rampravesh <rampraveshkr4545@gmail.com>
2025-10-06 15:30:11 +0530

    feat: Add switch statement examples

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,43 @@
+// switch
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case execute");
+        break;
+}
+
+const mon = "march"
+
+switch (mon) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case execute");
+        break;
+}
