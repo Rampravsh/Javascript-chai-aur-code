37fd6eaf81b0c357724e28f25d5837e7e243613c
rampravesh <rampraveshkr4545@gmail.com>
2025-10-10 11:33:24 +0530

    refactor: Comment out initial console logs and DOM manipulation code

--- 06.DOM manupulation/index.js
+++ 06.DOM manupulation/index.js
@@ -1,7 +1,4 @@
-console.log("hello world!");
-
-let a = document.querySelector("h1");
-console.log(a);
+// console.log("hello world!");
 
 // function clickHandler(e) {
 //   console.log("button was clicked");
@@ -9,3 +6,22 @@ console.log(a);
 
 // let b = document.querySelector("button");
 // b.addEventListener("onClick", clickHandler());
+
+
+// let a = document.querySelector("h1");
+// console.log(a);
+
+// a.innerHTML = "hello rampravesh kumar";
+
+// let p = document.querySelector("p");
+// p.innerHTML = "kya kar rahe ho bro ";
+
+// button = document.querySelector("button");
+
+// button.innerHTML = "new me ";
+// console.log(button);
+
+// button.style.backgroundColor = "#e4c6c6ff";
+// a.style.color = 'black'
+
+
