a41d8958484c8aedfe9633fad88e834218bff4f1
rampravesh <rampraveshkr4545@gmail.com>
2025-10-10 10:59:36 +0530

    feat: Add initial HTML, CSS, and JS files for DOM manipulation project

---
+++ 06.DOM manupulation/index.js
@@ -0,0 +1,11 @@
+console.log("hello world!");
+
+let a = document.querySelector("h1");
+console.log(a);
+
+// function clickHandler(e) {
+//   console.log("button was clicked");
+// }
+
+// let b = document.querySelector("button");
+// b.addEventListener("onClick", clickHandler());
