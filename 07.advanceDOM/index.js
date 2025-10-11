6523d3aef1bcfa8540e3c8e2223003fc8ab4a442
rampravesh <rampraveshkr4545@gmail.com>
2025-10-11 11:39:39 +0530

    refactor: Clean up commented-out code in index.js and organize HTML structure in index.html

--- 07.advanceDOM/index.js
+++ 07.advanceDOM/index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+// console.log("hello");
+// console.log(document);
+// console.dir(document);
+// const div = document.querySelectorAll("div");
+// div.forEach((div) => {
+//   console.log(div);
+// });
+
+// const div = document.getElementsByTagName("div");
+
+// console.dir(div);
+
+// const node = document.querySelector("#node");
+// console.log(node);
+
+// console.log(node.childNodes);
+
+// console.log(node.firstChild);
