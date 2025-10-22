114d417015f7b42d053c7067452242d057195b45
rampravesh <rampraveshkr4545@gmail.com>
2025-10-22 10:40:06 +0530

    feat: Add initial file for Functional Programming

---
+++ 14.Functional Programming/index.js
@@ -0,0 +1,23 @@
+// console.log("hello rampravesh");
+/*
+ Imperative foreach vs declarative foreach
+ Imperative map vs declarative map
+ Imperative filter vs declarative filter
+ Imperative reduce vs declarative reduce
+ Imperative every vs declarative every
+ Imperative find vs declarative find */
+
+// imperative programming
+/* let arr = [1, 2, 4, 5, 3, 5, 6];
+let double = [];
+for (let index = 0; index < arr.length; index++) {
+  double.push(arr[index] * 2);
+}
+console.log(double); */
+
+//declarative Programming
+
+/* let arr = [1, 2, 4, 5, 3, 5, 6];
+let double = arr.map((element) => element * 2);
+console.log(double); */
+ 
