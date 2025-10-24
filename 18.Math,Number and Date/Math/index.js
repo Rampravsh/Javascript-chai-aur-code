e460e7b59c26c0f698d66304e16561909947af0a
rampravesh <rampraveshkr4545@gmail.com>
2025-10-24 11:36:15 +0530

    MATH IN JAVASCRIPT

---
+++ 18.Math,Number and Date/Math/index.js
@@ -0,0 +1,7 @@
+//MATH IN JAVASCRIPT
+
+//Math is a built-in object that has properties and methods for mathematical
+//constants and functions. It is not a function object.
+//Math is used to perform mathematical tasks on numbers.
+
+//Example:
