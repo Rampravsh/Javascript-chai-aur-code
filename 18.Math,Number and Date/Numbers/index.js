219edcc91b2a984de3bf9798640746f9c844a3ca
rampravesh <rampraveshkr4545@gmail.com>
2025-10-24 11:05:31 +0530

    🟢NUMBER IN JAVASCRIPT

---
+++ 18.Math,Number and Date/Numbers/index.js
@@ -0,0 +1,22 @@
+// 🟢NUMBER IN JAVASCRIPT
+
+let num = new Number(1232);
+console.log(num);
+
+// Number Properties
+console.log("Number.MAX_VALUE: " + Number.MAX_VALUE);
+console.log("Number.MIN_VALUE: " + Number.MIN_VALUE);
+console.log("Number.EPSILON: " + Number.EPSILON);
+console.log("Number.MAX_SAFE_INTEGER: " + Number.MAX_SAFE_INTEGER);
+console.log("Number.MIN_SAFE_INTEGER: " + Number.MIN_SAFE_INTEGER);
+console.log("Number.NaN: " + Number.NaN);
+
+
+// Number Methods
+let num2 = 123.456;
+
+console.log("toFixed(): " + num2.toFixed(2));
+console.log("toPrecision(): " + num2.toPrecision(4));
+console.log("toExponential(): " + num2.toExponential(2));
+console.log("toString(): " + num2.toString());
+console.log("toLocaleString(): " + num2.toLocaleString());
