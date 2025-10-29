4a4d6ed68c4f341ad31e52566162bf201a15229f
rampravesh <rampraveshkr4545@gmail.com>
2025-10-29 11:17:09 +0530

    Asynchronous Programming

---
+++ 17.string array object/array/index.js
@@ -0,0 +1,13 @@
+// ARRAY IN JAVASCRIPT
+//an array is a list that can store multiple values in one place
+// Think of it like a row of boxes.where each box can hold one item (like a number word etc )
+//You can use it to group similar thing together
+
+//Create array using array Literal method (recommended):
+let colors = ["red", "blue", "green"];
+let marks = [23, 84, 34, 56];
+let personDetaols = ["riy", 10, "bihar"];
+
+//create array using array constructor:
+let arr = new Array("spiderman", "ironman", "thor");
+ 
