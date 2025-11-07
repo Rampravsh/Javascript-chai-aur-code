30d5329f126f0189098a5e96fbeb5a33ef4e0eaa
Rampravesh <rampraveshkr4545@gmail.com>
2025-11-08 03:05:57 +0530

    2nd Question done

--- js practice question/index.js
+++ js practice question/index.js
@@ -1,5 +1,6 @@
 // 1️⃣ create a function that returns the last element of an array.
-let arr = [1, 2, 3, 4, 5, 6];
+
+/* let arr = [1, 2, 3, 4, 5, 6];
 const lastElement = () => {
   for (let i = 0; i < arr.length; i++) {
     if (i === arr.length - 1) {
@@ -7,4 +8,12 @@ const lastElement = () => {
     }
   }
 };
-console.log(lastElement())
+console.log(lastElement()) */
+
+//2️⃣ Find the combination of two arrays.
+
+const arr1 = [1, 2, 3, 4, 5, 6];
+const arr2 = ["a", "b", "c"];
+
+let newArr = [...arr1, ...arr2];
+console.log(newArr)
