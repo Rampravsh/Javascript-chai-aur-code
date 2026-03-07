b6c17dc20fe3a66a5756d1949fde65426316e882
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-07 09:47:25 +0530

    add initial HTML structure for OOP examples and enhance debugging input form

--- 16.OOPs/index.js
+++ 16.OOPs/index.js
@@ -1,2 +1,82 @@
-let date = new Date();
-console.log(date);
+// const root=document.getElementById("root")
+
+// let date = new Date();
+// console.log(date.toLocaleDateString());
+// root.innerText=date.toDateString()
+
+// let Student = {
+//   name: "rampravesh",
+//   age: 21,
+//   passion: "bkaiti",
+//   introduceMySelf: function () {
+//     console.log(`my name is ${this.name},my age is ${this.age}`);
+//   },
+// };
+
+// console.log(Student)
+
+// Student.introduceMySelf()
+
+// let date1 = new Date('2025')
+
+// console.log(date1)
+
+// factory function
+
+// function Student(name, age, passion) {
+//   return {
+//     myName: name,
+//     age,
+//     passion,
+//     introduceMySelf: function () {
+//       console.log(`my name is ${this.name},my age is ${this.age}`);
+//     },
+//   };
+// }
+
+// const result = Student("rampravesh", 20, "study");
+// const result2 = Student("ritika", 2, "play");
+// console.log(result);
+// console.log(result2);
+
+// constructor function
+
+// function Student(name,age, passion){
+//     this.name=name
+//     this.age=age
+//     this.passion=passion
+//     return this
+// }
+
+// let s1=new Student('rampravesh',21,'bakati')
+
+// let s2=new Student('abhilasha',21,'cooking')
+
+// console.log(s1)
+// console.log(s2)
+
+// class Student {
+//   name = "rampravesh";
+//   age = 20;
+//   passion = "bakaiti";
+// }
+
+// let s1 = new Student();
+// let s2 = new Student();
+
+// console.log(s1)
+// console.log(s2)
+
+// class Student{
+//     constructor(name,age,passion){
+//         this.name=name;
+//         this.age=age;
+//         this.passion=passion;
+//     }
+// }
+
+// let s1=new Student('rampravesh',20,'bkaiti');
+
+// console.log(s1)
+
+
