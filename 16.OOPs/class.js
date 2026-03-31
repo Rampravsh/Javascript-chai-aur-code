fc735d25b40a1a8858906218d7d032091c1c6a13
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-31 19:51:19 +0530

    refactor Car class to use private fields and methods; update start method to burn fuel

--- 16.OOPs/class.js
+++ 16.OOPs/class.js
@@ -1,21 +1,38 @@
-class Car {
-  constructor(name, color, mileage) {
-    this.name = name;
-    this.color = color;
-    this.mileage = mileage;
-  }
+// class Car {
+//   constructor(name, color, mileage) {
+//     this.name = name;
+//     this.color = color;
+//     this.mileage = mileage;
+//   }
 
-  start() {
-    console.log(`${this.name} is starting...`);
+//   start() {
+//     console.log(`${this.name} is starting...`);
+//   }
+//   stop() {
+//     console.log(`${this.name} is stoping...`);
+//   }
+// }
+
+// let Bmw = new Car("BMW", "dark", 45);
+// let toyota = new Car("Toyota", "Silver Blue", 45);
+// let buggati = new Car("Buggati", "Brown", 45);
+
+// console.log(Bmw,toyota,buggati)
+
+class Car {
+  #fuel = 100;
+
+  #burnFuel() {
+    this.#fuel -= 1;
   }
-  stop() {
-    console.log(`${this.name} is stoping...`);
+  start() {
+    this.#burnFuel();
+    console.log("car is Starting");
   }
 }
 
-let Bmw = new Car("BMW", "dark", 45);
-let toyota = new Car("Toyota", "Silver Blue", 45);
-let buggati = new Car("Buggati", "Brown", 45);
+let buggati = new Car();
 
-
-console.log(Bmw,toyota,buggati)
+console.log(buggati);
+buggati.start();
+console.log(buggati);
